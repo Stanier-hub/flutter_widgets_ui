0f280aaa7e2e1d2d9220f26958decadf49a94181
StanierHub <stanier_ts@outlook.com>
2025-07-30 00:50:46 -0300

    fiz: correcao servico padrao

--- lib/service/servico_padrao.dart
+++ lib/service/servico_padrao.dart
@@ -1,28 +1,9 @@
 abstract class SevicePadrao<T> {
-  Future<T> criar({required Map<String, dynamic> dados}) async {
-    throw UnimplementedError();
-  }
-
+  Future<T> criar({required Map<String, dynamic> dados});
   Future<T?> atualizar({
     required T modelo,
     required Map<String, dynamic> dados,
-  }) async {
-    throw UnimplementedError();
-  }
-
-  Future<List<T>> buscar({Map<String, dynamic>? filtros}) async {
-    throw UnimplementedError();
-  }
-
-  Future<void> excluir({required T modelo}) async {
-    throw UnimplementedError();
-  }
-
-  Future<T?> buscarPorId({required String id}) async {
-    throw UnimplementedError();
-  }
-
-  Future<void> baixar({Map<String, dynamic>? filtros}) async {
-    throw UnimplementedError();
-  }
+  });
+  Future<List<T>> buscar({Map<String, dynamic>? filtros});
+  Future<dynamic> excluir({required int id});
 }
